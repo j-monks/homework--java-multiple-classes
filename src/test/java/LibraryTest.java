c9a641e6911f984e2d09604dd72d64944377dd02
James Monks <J.monks23@gmail.com>
2020-08-25 16:11:04 +0100

    library can now add book to stock (booklist)

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+        Library library;
+        Book book1;
+        Book book2;
+        Book book3;
+
+        @Before
+        public void setUp() {
+            library = new Library();
+            book1 = new Book("Harry Potter","JK","Fiction");
+            book2 = new Book("1984","George Orwell","Fiction");
+            book3 = new Book("Dune","Frank Herbert","Sci/fi");
+        }
+
+        @Test
+        public void countBooks() {
+            assertEquals(0, library.bookCount());
+        }
+
+        @Test
+        public void addBookToStock() {
+            library.addBook(book1);
+            assertEquals(1, library.bookCount());
+        }
 }
